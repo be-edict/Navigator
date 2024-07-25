be90dccb8165463ca5f64953d8f17feb478f20c6
Benedict <breinhard75@gmail.com>
2024-07-25 10:06:22 +0200

    Insert elements from json file into link_container

--- script/tableparser.js
+++ script/tableparser.js
@@ -1,13 +1,20 @@
 function buildTable(tableName) {
 
-    const container = $("#link_container");
-    const newChildren = [];
+    const container = document.getElementById("link_container");
 
     fetch("../ressource/page/" + tableName + ".json")
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            const newChildren = [];
+            data[tableName].forEach((value, index) => {
+                newChildren[index] = document.createElement('div');
+                let link = document.createElement('a');
+                link.href = value.href;
+                link.title = value.title;
+                newChildren[index].appendChild(link);
+            });
+            return newChildren;
+        })
+        .then((arr) => container.replaceChildren(...arr))
         .catch((error) => console.log(error));
-
-
-    container.replaceChildren(...newChildren);
 }
