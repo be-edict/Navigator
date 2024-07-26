7710c27270c1a7f6914dd27a9f24bb2f8990cb48
Benedict <breinhard75@gmail.com>
2024-07-26 14:08:36 +0200

    Navigate to linkset on click

--- script/createDynMenu.js
+++ script/createDynMenu.js
@@ -4,6 +4,10 @@ async function buildMenu(objs) {
     for (let item of objs) {
         let key = "";
         let li = document.createElement("li");
+        li.onclick = (e) => {
+            e.stopPropagation();
+            buildTable(e.currentTarget.firstElementChild.textContent+".json");
+        };
         let a = document.createElement("a");
         await fetch("./ressource/page/" + item.name)
             .then((res) => res.json())
@@ -15,4 +19,33 @@ async function buildMenu(objs) {
         menuItems.push(li);
     }
     menu.replaceChildren(...menuItems);
+}
+
+function buildTable(tableName) {
+
+    const container = document.getElementById("link_container");
+
+    fetch("./ressource/page/" + tableName)
+        .then((response) => response.json())
+        .then((data) => {
+            const newChildren = [];
+            data[tableName.match(/[^\.]*/)[0]].forEach((value, index) => {
+                newChildren[index] = document.createElement('a');
+                newChildren[index].href = value.href;
+                //creating the div
+                let divElement = document.createElement('div');
+                newChildren[index].appendChild(divElement);
+                //creating the icon
+                let icon = document.createElement('img');
+                icon.src = value.href.match(/https?:\/\/[^\/]+/)[0] + "/favicon.ico";
+                divElement.appendChild(icon);
+                //creating p
+                let p = document.createElement('p');
+                p.innerText = value.title;
+                divElement.appendChild(p);
+            });
+            return newChildren;
+        })
+        .then((arr) => container.replaceChildren(...arr))
+        .catch((error) => console.log(error));
 }
